19be4808cdb400107d1705d16d9001f06b7e3792
Mustafa Sezgi <76861453+msezgi38@users.noreply.github.com>
2022-06-09 12:24:09 +0300

    Add files via upload

---
+++ araba-sitesi-2/script.js
@@ -0,0 +1,106 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+  searchForm.classList.toggle('active');
+  shoppingCart.classList.remove('active');
+  loginForm.classList.remove('active');
+  navbar.classList.remove('active');
+}
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () => {
+  loginForm.classList.toggle('active');
+  searchForm.classList.remove('active');
+  shoppingCart.classList.remove('active');
+  navbar.classList.remove('active');
+}
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+  navbar.classList.toggle('active');
+  searchForm.classList.remove('active');
+  shoppingCart.classList.remove('active');
+  loginForm.classList.remove('active');
+}
+
+window.onscroll = () => {
+  searchForm.classList.remove('active');
+  shoppingCart.classList.remove('active');
+  loginForm.classList.remove('active');
+  navbar.classList.remove('active');
+}
+var swiper = new Swiper(".vehicles-slider", {
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+var swiper = new Swiper(".featured-slider", {
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+var swiper = new Swiper(".review-slider", {
+  grabCursor: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
